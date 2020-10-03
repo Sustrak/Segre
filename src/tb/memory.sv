9765800d114cdd19a2965b50b1b61aa242bc3d13
Josep Sans <josepsansprats@gmail.com>
2020-10-03 19:34:38 +0200

    Added verbosity to tb

--- src/tb/memory.sv
+++ src/tb/memory.sv
@@ -14,9 +14,9 @@ module memory (
     output logic [WORD_SIZE-1:0] data_o
 );
 
-localparam NUM_WORDS = 1024 * 64; // 64Kb
-localparam TEXT_REGION = 0;
-localparam DATA_REGION = 1024*32;
+parameter NUM_WORDS = 1024 * 64; // 64Kb
+parameter TEXT_REGION = 0;
+parameter DATA_REGION = 1024*32;
 
 logic [7:0] mem [NUM_WORDS-1:0];
 
@@ -24,24 +24,19 @@ logic [WORD_SIZE-1:0] rd_data;
 
 int num_of_instructions = 0;
 
+int hex_file_fd;
+
 initial begin
     static int ret, addr = 0, fd;
     static string line, hex_file_name;
 
     // Read the hex file path and open the file in read mode
-    if (!$value$plusargs("HEX_FILE=%s", hex_file_name))
-        `uvm_fatal(get_type_name(), "Start the simulation with +HEX_FILE to load the test into the memory")
-
-    fd = $fopen(hex_file_name, "r");
-    if (!fd)
-        `uvm_fatal(get_type_name(), $sformatf("%s, couldn't be opened", hex_file_name))
-
     num_of_instructions = 0;
 
-    while (!$feof(fd)) begin
-        if ($fgets(line, fd)) begin
+    while (!$feof(hex_file_fd)) begin
+        if ($fgets(line, hex_file_fd)) begin
             assert (addr < DATA_REGION) else `uvm_fatal(get_type_name(), ".text was about to get written in .data section")
-            `uvm_info(get_type_name(), $sformatf("Writting in %h the data %d", addr, line), UVM_DEBUG);
+            `uvm_info(get_type_name(), $sformatf("Writting in %h the data %d", addr, line), UVM_LOW);
             mem[addr]   = line.substr(6, 7).atohex();
             mem[addr+1] = line.substr(4, 5).atohex();
             mem[addr+2] = line.substr(2, 3).atohex();
@@ -80,9 +75,7 @@ end
 always @(posedge clk_i) data_o <= rd_data;
 
 task memory_verbose;
-    if (rd_i && addr_i < DATA_REGION)
-        `uvm_info(get_type_name(), $sformatf("Reading Instruction: %s (%h)", "NOT_IMPLEMENTED", rd_data), UVM_INFO)
-    if (rd_i && addr_i >= DATA_REGION)
+    if (rd_i)
         `uvm_info(get_type_name(), $sformatf("Reading data: %h from %h", rd_data, addr_i), UVM_INFO)
     if (wr_i)
         `uvm_info(get_type_name(), $sformatf("Writing %s: %h at %h", data_type_i, data_i, addr_i), UVM_INFO)
--- src/tb/top_tb.sv
+++ src/tb/top_tb.sv
@@ -8,6 +8,8 @@ import uvm_pkg::*;
 import "DPI-C" function string decode_instruction(unsigned bits);
 import "DPI-C" function void free_ptr(chandle ptr);
 
+localparam NUM_REGS = 2**REG_SIZE;
+
 module top_tb;
 
     timeunit 1ns;
@@ -16,6 +18,9 @@ module top_tb;
     logic clk;
     logic clk_mem;
     logic rsn;
+
+    string test_name;
+    int result_file_fd;
 
     segre_core_if_t segre_core_if();
 
@@ -45,6 +50,23 @@ module top_tb;
     );
 
     initial begin
+        // Check for test files and setup fds for the test bench and memory
+        if (!$value$plusargs("TEST_NAME=%s", test_name))
+            `uvm_fatal(get_type_name(), "Couldn't find the TEST_NAME argument, please provide it with +TEST_NAME=<testname>")
+        else
+            `uvm_info(get_type_name(), $sformatf("Starting test: %s", test_name), UVM_LOW)
+
+        tb_mem.hex_file_fd = $fopen($sformatf("../../tests/hex_segre/%s.hex", test_name), "r");
+        if (!tb_mem.hex_file_fd)
+            `uvm_fatal(get_type_name(), $sformatf("Couldn't find the hex file for %s", test_name))
+
+        result_file_fd = $fopen($sformatf("../../tests/result_segre/%s.result", test_name), "r");
+        if (!result_file_fd)
+            `uvm_warning(get_type_name(), $sformatf("Couldn't find the result file for %s", test_name))
+        
+    end
+
+    initial begin
         clk <= 0;
         clk_mem <= 0;
         rsn <= 0;
@@ -56,8 +78,51 @@ module top_tb;
     initial begin
         repeat(2) @(posedge clk);
         rsn <= 1;
-        repeat(tb_mem.num_of_instructions) repeat(5) @(posedge clk);
+        fork
+            run_tb;
+            monitor_tb;
+        join_any
+        check_results;
+        `uvm_info(get_type_name(), "End Of Test", UVM_LOW)
         $finish;
     end
+
+    task run_tb begin
+        while(keep_running_tb) @(posedge clk);
+    endtask
+
+    function bit keep_running_tb;
+        if (segre_core_if.addr < tb_mem.DATA_REGION && segre_core_if.mem_rd_data == 32'hfff01073)
+            return 0;
+        return 1;
+    endfunction
+
+    function void check_results;
+        int golden_results [32];
+        int counter = 0;
+        logic [WORD_SIZE-1:0][NUM_REGS-1:0] segre_rf;
+        string line;
+
+        assign segre_rf = dut.segre_register_file.rf_reg;
+
+        // Read results from file
+        while (!$feof(result_file_fd)) begin
+            if ($fgets(line, result_file_fd)) begin
+                golden_results[counter] = line.atohex();
+            end
+        end 
+    endfunction
+
+    task monitor_tb begin
+        `uvm_info(get_type_name(), "Starting tb monitor", UVM_LOW)
+        forever begin
+            @(segre_core_if.mem_rd);
+            if (segre_core_if.addr < tb_mem.DATA_REGION) begin
+                string instr_decoded = decode_instruction(segre_core_if.mem_rd_data);
+                `uvm_info(get_type_name(), $sformatf("PC: 0x%0h: %s (0x%0h) "), segre_core_if.addr, instr_decoded, segre_core_if.mem_rd_data)
+            end
+        end
+        `uvm_fatal(get_type_name(), "Shouldn't have reach this part of the monitor_tb")
+    endtask
 
 endmodule
