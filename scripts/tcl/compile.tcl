92b989da2313950df129fbf69d711a8bd2cd1b84
Josep Sans <josepsansprats@gmail.com>
2020-10-20 20:06:12 +0200

    Test CI

--- scripts/tcl/compile.tcl
+++ scripts/tcl/compile.tcl
@@ -33,7 +33,7 @@ vlog -sv -work $work_dir $rtl_dir/segre_core.sv
 
 # Compile tb
 vlog -sv -work $work_dir $tb_dir/interface.sv
-set GITHUB_CI [string range $GITHUB_CI 0 3]
+set GITHUB_CI [string range $GITHUB_CI 0 4]
 if {$GITHUB_CI == "/_w/" } {
     vlog -sv -work $work_dir -define GITHUB_CI $tb_dir/memory.sv
     vlog -sv -work $work_dir -define GITHUB_CI $tb_dir/top_tb.sv
