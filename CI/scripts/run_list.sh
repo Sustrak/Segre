4f22be75e535b11a48f6ef5c76066a40410db2f0
Josep Sans <josepsansprats@gmail.com>
2020-10-21 15:23:16 +0200

    CI test

--- CI/scripts/run_list.sh
+++ CI/scripts/run_list.sh
@@ -2,13 +2,13 @@
 ### Will launch the tests passed as list               ###
 ##########################################################
 
-segre_dir=$(git rev-parse --show-toplevel)
+#segre_dir=$(git rev-parse --show-toplevel)
+segre_dir=.
 echo "Executing tests..."
 for t in $@; do
     vsim -c -nolog -do "do $segre_dir/scripts/tcl/compile.tcl $t; quit" > /dev/null
     rm -rf vsim.wlf
-    if grep -q "UVM_ERROR\|UVM_FATAL" $segre_dir/build/sim_transcript; then
+    if grep -q "Error\|Fatal\|UVM_ERROR\|UVM_FATAL" $segre_dir/build/sim_transcript; then
         echo "$t test has failed"
     fi
-    exit 0
 done
