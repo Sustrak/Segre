d1b2a7821a52b5d5cd4bab265ffc5cf5e67958e5
Josep Sans <josepsansprats@gmail.com>
2020-10-21 15:29:57 +0200

    CI test

--- CI/scripts/run_list.sh
+++ CI/scripts/run_list.sh
@@ -4,11 +4,15 @@
 
 #segre_dir=$(git rev-parse --show-toplevel)
 segre_dir=.
+failed=0
 echo "Executing tests..."
 for t in $@; do
     vsim -c -nolog -do "do $segre_dir/scripts/tcl/compile.tcl $t; quit" > /dev/null
     rm -rf vsim.wlf
     if grep -q "Error\|Fatal\|UVM_ERROR\|UVM_FATAL" $segre_dir/build/sim_transcript; then
         echo "$t test has failed"
+        failed=1
     fi
 done
+
+exit $failed
